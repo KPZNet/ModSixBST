47df1439e0e9fab503386759d6c7a08b0f4bff37
KPZNET <kpznet@outlook.com>
2021-08-23 18:48:33 -0700

    mcDev

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from BSTree import BSTree
 
 keys = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+print("Input List")
+print(keys)
 bst = BSTree()
 bst.BuildTree(keys)
 bst.print_tree("Initial Tree")
