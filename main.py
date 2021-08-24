6aab0a9011313c0a3a601e49bca7e867992ccee6
KPZNET <kpznet@outlook.com>
2021-08-23 18:53:59 -0700

    mcDev

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from BSTree import BSTree
+def PL(l = 1):
+    for i in range(l):
+        print("\n")
+
 
 keys = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 print("Input List")
 print(keys)
+PL()
 bst = BSTree()
 bst.BuildTree(keys)
 bst.print_tree("Initial Tree")
+PL()
 bst.rebalance()
 bst.print_tree("Balanced Tree")
 
